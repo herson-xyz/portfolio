8f6e8ada264a1e66099cf8db17ad62a7453ccaba
watsrunner <watsrunner@protonmail.com>
2021-11-16 22:37:44 -0800

    test

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('listening at ' + port));
 app.use(express.static('public'));
 
-// app.get('/.well-known/acme-challenge/:content', function(req, res) {
-//     res.send('J-XBhKLq6jp86ixjaLGw20U4KzkrzWQ6vkEW6U8SiQM.k9l5F51z8IaUgTLz7bZd-unw6t2WWsF9-bygm0ZVkvA')
-//   })
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+    res.send('J-XBhKLq6jp86ixjaLGw20U4KzkrzWQ6vkEW6U8SiQM.k9l5F51z8IaUgTLz7bZd-unw6t2WWsF9-bygm0ZVkvA')
+  })

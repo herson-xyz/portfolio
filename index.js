fa0505c20b321e33dc196fb101c22058f3153ee4
watsrunner <watsrunner@protonmail.com>
2021-11-16 18:22:54 -0800

    test

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const app = express();
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('listening at ' + port));
 app.use(express.static('public'));
+
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+    res.send('J-XBhKLq6jp86ixjaLGw20U4KzkrzWQ6vkEW6U8SiQM.k9l5F51z8IaUgTLz7bZd-unw6t2WWsF9-bygm0ZVkvA')
+  })
